203b5c2be25da37d93288dbebe6fd44c3f800f16
Sam Halliday <sam.halliday@gmail.com>
2018-08-10 16:16:12 +0100

    all kinds of crazy

--- modules/core/src/main/scala/iota/internal/ProductMacros.scala
+++ modules/core/src/main/scala/iota/internal/ProductMacros.scala
@@ -17,79 +17,76 @@ import scalaz.NonEmptyList
 import scalaz.std.list._
 //#-scalaz
 
+final class ProductSeq(p: Product) extends OptimisedIndexedSeq[Any] {
+  def apply(i: Int): Any = p.productElement(i)
+  def length: Int = p.productArity
+}
+
+final class ArraySeq(p: collection.mutable.WrappedArray[Any])
+    extends OptimisedIndexedSeq[Any] {
+  def apply(i: Int): Any = p(i)
+  def length: Int = p.length
+}
+
+sealed abstract class OptimisedIndexedSeq[A]
+    extends collection.immutable.IndexedSeq[A] {
+
+  // these things can live in scalaz-deriving... here for now
+  // fusion of zip and map
+  def zipmap[B, C](bs: OptimisedIndexedSeq[B])(f: (A, B) => C): List[C] = {
+    var lst: List[C] = Nil
+    var i = length - 1
+    while (i >= 0) {
+      lst ::= f(apply(i), bs(i))
+      i -= 1
+    }
+    lst
+  }
+  def zipmap2[B1, B2, C](
+    b1s: OptimisedIndexedSeq[B1],
+    b2s: OptimisedIndexedSeq[B2]
+  )(f: (A, B1, B2) => C): List[C] = {
+    var lst: List[C] = Nil
+    var i = length - 1
+    while (i >= 0) {
+      lst ::= f(apply(i), b1s(i), b2s(i))
+      i -= 1
+    }
+    lst
+  }
+
+  // optimisations...
+  override def toList: List[A] = {
+    var lst: List[A] = Nil
+    var i = length - 1
+    while (i >= 0) {
+      lst ::= apply(i)
+      i -= 1
+    }
+    lst
+  }
+  override def foldRight[B](z: B)(op: (A, B) ⇒ B): B = {
+    var acc = z
+    var i = length - 1
+    while (i >= 0) {
+      acc = op(apply(i), acc)
+      i -= 1
+    }
+    acc
+  }
+  override def foldLeft[B](z: B)(op: (B, A) ⇒ B): B = {
+    var acc = z
+    var i = 0
+    val len = length
+    while (i < len) {
+      acc = op(acc, apply(i))
+      i += 1
+    }
+    acc
+  }
+}
+
 private[iota]  //#=cats
-final class ProductSeq(p: Product)
-    extends collection.immutable.IndexedSeq[Any] {
-  def apply(i: Int): Any = p.productElement(i)
-  def length: Int = p.productArity
-
-  // optimisations...
-  override def toList: List[Any] = {
-    var lst: List[Any] = Nil
-    var i = length - 1
-    while (i >= 0) {
-      lst ::= apply(i)
-      i -= 1
-    }
-    lst
-  }
-  override def foldRight[B](z: B)(op: (Any, B) ⇒ B): B = {
-    var acc = z
-    var i = length - 1
-    while (i >= 0) {
-      acc = op(apply(i), acc)
-      i -= 1
-    }
-    acc
-  }
-  override def foldLeft[B](z: B)(op: (B, Any) ⇒ B): B = {
-    var acc = z
-    var i = 0
-    val len = length
-    while (i < len) {
-      acc = op(acc, apply(i))
-      i += 1
-    }
-    acc
-  }
-}
-
-// an efficient Seq implementation for an , for use in a Prod
-final class ArraySeq(p: Product) extends collection.immutable.IndexedSeq[Any] {
-  def apply(i: Int): Any = p.productElement(i)
-  def length: Int = p.productArity
-
-  // optimisations...
-  override def toList: List[Any] = {
-    var lst: List[Any] = Nil
-    var i = length - 1
-    while (i >= 0) {
-      lst ::= apply(i)
-      i -= 1
-    }
-    lst
-  }
-  override def foldRight[B](z: B)(op: (Any, B) ⇒ B): B = {
-    var acc = z
-    var i = length - 1
-    while (i >= 0) {
-      acc = op(apply(i), acc)
-      i -= 1
-    }
-    acc
-  }
-  override def foldLeft[B](z: B)(op: (B, Any) ⇒ B): B = {
-    var acc = z
-    var i = 0
-    val len = length
-    while (i < len) {
-      acc = op(acc, apply(i))
-      i += 1
-    }
-    acc
-  }
-}
-
 private[iotaz] //#=scalaz
 final class ProductMacros(val c: Context) {
   import c.universe._
@@ -118,8 +115,10 @@ final class ProductMacros(val c: Context) {
       _        <- Traverse[List].traverse(argTypes zip algebras)(tpes =>
                     require(tpes._1 <:< tpes._2,
                       s"Expected ${tpes._1} <:< ${tpes._2}").toAvowal).toEither
-      seq       = if (argTypes.length <= 22) q"$pkg.ProductSeq((..$args))"
-                  else q"_root_.scala.collection.immutable.IndexedSeq[_root_.scala.Any](..$args)"
+      seq       = if (argTypes.length == 0) q"_root_.scala.collection.immutable.Nil"
+                  else if (argTypes.length == 1) q"new $pkg.ProductSeq(_root_.scala.Tuple1(..$args))"
+                  else if (argTypes.length <= 22) q"new $pkg.ProductSeq((..$args))"
+                  else q"new $pkg.ArraySeq(_root_.scala.Array[_root_.scala.Any](..$args))"
    } yield q"${tb.iotaPackage}.Prod.unsafeApply[$L]($seq)")
   }
 
